49e0522480b6d3ef85fd89a8446f3a124b238871
pablodiste <pablodiste@gmail.com>
2021-12-22 16:08:12 -0300

    Testing NoKey combinations

--- app/src/main/java/com/pablodiste/android/sample/network/StarWarsService.kt
+++ app/src/main/java/com/pablodiste/android/sample/network/StarWarsService.kt
@@ -1,10 +1,14 @@
 package com.pablodiste.android.sample.network
 
 import com.pablodiste.android.sample.models.realm.People
+import com.pablodiste.android.sample.models.realm.Planet
 import retrofit2.http.GET
 
 interface StarWarsService {
 
     @GET("people/")
     suspend fun getPeople(): ApiListResponse<People>
+
+    @GET("planets/")
+    suspend fun getPlanets(): ApiListResponse<Planet>
 }
--- app/src/main/java/com/pablodiste/android/sample/ui/main/MainViewModel.kt
+++ app/src/main/java/com/pablodiste/android/sample/ui/main/MainViewModel.kt
@@ -3,29 +3,44 @@ package com.pablodiste.android.sample.ui.main
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.pablodiste.android.datastore.closable.*
+import com.pablodiste.android.datastore.closable.stream
 import com.pablodiste.android.sample.models.realm.People
 import com.pablodiste.android.sample.repositories.store.PeopleStore
+import com.pablodiste.android.sample.repositories.store.PlanetsStore
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
-    private val peopleStore = PeopleStore(viewModelScope)
+    private val peopleStore1 = PeopleStore(viewModelScope)
+    private val peopleStore2 = PeopleStore(viewModelScope)
+    private val planetStore = PlanetsStore(viewModelScope)
     val uiState = MutableStateFlow<List<People>>(listOf())
 
-    fun getPeople() = viewModelScope.launch {
-
-        peopleStore.stream(refresh = true).map { it.value }.collect { people ->
-            Log.d(TAG, "Received new People")
-            people.forEach { Log.d(TAG, "People: $it") }
-            uiState.value = people
+    fun getPeople() {
+        viewModelScope.launch {
+            peopleStore1.stream(refresh = true).collect { result ->
+                Log.d(TAG, "Stream 1: ${result.origin} Received new People")
+                //people.forEach { Log.d(TAG, "People: $it") }
+                uiState.value = result.value
+            }
+        }
+        viewModelScope.launch {
+            peopleStore2.stream(refresh = true).collect { result ->
+                Log.d(TAG, "Stream 2: ${result.origin} Received new People")
+                //people.forEach { Log.d(TAG, "People: $it") }
+                uiState.value = result.value
+            }
+        }
+        viewModelScope.launch {
+            planetStore.stream(refresh = true).collect { result ->
+                Log.d(TAG, "Stream 3: ${result.origin} Received new Planets")
+            }
         }
         //uiState.value = peopleStore.get().value
-        peopleStore.something()
+        peopleStore1.something()
 
         viewModelScope.coroutineContext[Job]?.invokeOnCompletion {
             Log.d(TAG, "onCompletion")
