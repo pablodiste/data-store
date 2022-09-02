c37836e0ebc9ff163ecb0c73f6c0d635ccfbb80c
pablodiste <pablodiste@gmail.com>
2022-09-02 18:20:09 -0300

    Adding some tests and docs

--- data-store/src/main/java/dev/pablodiste/datastore/Store.kt
+++ data-store/src/main/java/dev/pablodiste/datastore/Store.kt
@@ -3,22 +3,74 @@ package dev.pablodiste.datastore
 import dev.pablodiste.datastore.ratelimiter.RateLimitPolicy
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * An store is a repository implementation, in charge of fetching data from a remote source and allowing queries through a single source
+ * of truth
+ */
 interface Store<K: Any, T: Any> {
+    /**
+     * Return a flow of data using the given key. The flow never completes.
+     * @param key Key to identify the request
+     * @param refresh When true it fetches data from the fetcher to update the source of truth
+     */
     fun stream(key: K, refresh: Boolean): Flow<StoreResponse<T>>
+
+    /**
+     * Returns data from the single source of truth using the given key.
+     */
     suspend fun get(key: K): StoreResponse<T>
+
+    /**
+     * Tries to fetch data from the fetcher.
+     * @param forced When true it always request new data from the fetcher.
+     */
     suspend fun fetch(key: K, forced: Boolean = true): StoreResponse<T>
 }
 
+/**
+ * Stores data locally
+ */
 interface SourceOfTruth<K: Any, T: Any> {
+    /**
+     * Return true when exists data in the source of truth with the provided key
+     */
     suspend fun exists(key: K): Boolean
+
+    /**
+     * Returns a flow that never completes that queries for data with the given key and listen for updates in the source of truth
+     */
     fun listen(key: K): Flow<@JvmWildcard T>
+
+    /**
+     * Stores the provided entity and returns the stored item
+     * @param entity What we want to store
+     * @param removeStale if true, it applies a staleness policy to delete entities before storing the new ones
+     */
     suspend fun store(key: K, entity: T, removeStale: Boolean = false): T
+
+    /**
+     * Makes a query using the provided key and return the result immediately.
+     */
     suspend fun get(key: K): T
+
+    /**
+     * Deletes the entities related to the provided key
+     */
     suspend fun delete(key: K): Boolean
 }
 
+/**
+ * Fetches data from a remote source, like an API
+ */
 interface Fetcher<K: Any, I: Any> {
+    /**
+     * Policy to limit the number of consecutive or concurrent calls
+     */
     val rateLimitPolicy: RateLimitPolicy
+
+    /**
+     * Fetches data from the remote source.
+     */
     suspend fun fetch(key: K): FetcherResult<I>
 }
 
---
+++ data-store/src/test/java/dev/pablodiste/datastore/StoreImplTest.kt
@@ -0,0 +1,89 @@
+package dev.pablodiste.datastore
+
+import app.cash.turbine.test
+import dev.pablodiste.datastore.impl.SimpleStoreImpl
+import dev.pablodiste.datastore.inmemory.InMemoryListSourceOfTruth
+import dev.pablodiste.datastore.inmemory.InMemorySourceOfTruth
+import dev.pablodiste.datastore.ratelimiter.FetchAlways
+import dev.pablodiste.datastore.ratelimiter.RateLimitPolicy
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import java.util.concurrent.TimeUnit
+
+@ExperimentalCoroutinesApi
+class StoreImplTest: CoroutineTest() {
+
+    data class Key(val cid: Int)
+    data class Entity(val id: Int, val cid: Int, val name: String)
+
+    private lateinit var store: Store<Key, List<Entity>>
+    private lateinit var fetcher: Fetcher<Key, List<Entity>>
+
+    @Before
+    fun prepare() {
+        fetcher = mock<Fetcher<Key, List<Entity>>> {
+            on { rateLimitPolicy } doReturn FetchAlways
+            onBlocking { fetch(Key(1)) } doReturn FetcherResult.Data(listOf(
+                Entity(1, 1, "One - First"),
+                Entity(2, 1, "One - Second"),
+                Entity(3, 1, "One - Third")
+            ))
+            onBlocking { fetch(Key(2)) } doReturn FetcherResult.Data(listOf(
+                Entity(1, 2, "Two - First"),
+                Entity(2, 2, "Two - Second"),
+                Entity(3, 2, "Two - Third"),
+                Entity(4, 2, "Two - Fourth")
+            ))
+        }
+        val sourceOfTruth = object: InMemoryListSourceOfTruth<Key, Entity>() {
+            override fun predicate(key: Key): (key: Key, value: Entity) -> Boolean = { key, value -> value.cid == key.cid }
+        }
+        store = SimpleStoreImpl(fetcher, sourceOfTruth)
+    }
+
+    @Test
+    fun storeGet() = runTest {
+        val result1 = store.get(Key(1))
+        assertEquals(3, result1.requireData().size)
+        val result2 = store.get(Key(2))
+        assertEquals(4, result2.requireData().size)
+    }
+
+    @Test
+    fun storeStream() = runTest {
+        store.stream(Key(1), refresh = true).test {
+            val data = awaitItem() as StoreResponse.Data
+            assertEquals(ResponseOrigin.FETCHER, data.origin)
+            cancelAndIgnoreRemainingEvents()
+        }
+        store.stream(Key(1), refresh = false).test {
+            val data = awaitItem() as StoreResponse.Data
+            assertEquals(ResponseOrigin.SOURCE_OF_TRUTH, data.origin)
+            cancelAndIgnoreRemainingEvents()
+        }
+        store.stream(Key(1), refresh = true).test {
+            val dataSOT = awaitItem() as StoreResponse.Data
+            assertEquals(ResponseOrigin.SOURCE_OF_TRUTH, dataSOT.origin)
+            val dataFetcher = awaitItem() as StoreResponse.Data
+            assertEquals(ResponseOrigin.FETCHER, dataFetcher.origin)
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun storeFetch() = runTest {
+        val result1 = store.fetch(Key(1))
+        assertEquals(3, result1.requireData().size)
+        val result2 = store.get(Key(1))
+        assertEquals(3, result2.requireData().size)
+    }
+
+}
